28da3cbada0f5cc8d73d93bfe317ed3033e094e5
Anil Anderson <aniland@unc.edu>
2022-11-14 16:08:19 -0500

    Added code to create new exploratory data analysis figures.

--- exploratory.R
+++ exploratory.R
@@ -1,99 +1,91 @@
 source("setup.R")
 
-## Code for my own analysis that I want make to ignore
-# health$CVDCRHD4
-# health %>% filter(CVDCRHD4 == 1) %>% nrow()
+agehist <- ggplot(health, aes(age)) + 
+  geom_histogram(binwidth = 5) + 
+  labs(x = "Age", y = "Count", title = "Histogram of Ages")
+ggsave("figures/agehist.png", plot = agehist, dpi = 300)
+# ggplot(health %>% filter(!(is.na(age_cat))), aes(age_cat)) + geom_bar()
 
-# ggplot(health, aes(AGE80)) + geom_histogram(binwidth = 4)
-# ggplot(health, aes(AGEG5YR)) + geom_bar()
-
-age_heart <- health %>% 
-  filter(CVDCRHD4 == 1 | CVDCRHD4 == 2) %>% 
-  select(CVDCRHD4, AGE80) %>% 
-  mutate(CVDCRHD4 = ifelse(CVDCRHD4 == 1, 1, 0)) %>% 
-  mutate(CVDCRHD4 = factor(CVDCRHD4))
-
-agebox <- ggplot(age_heart, aes(x=factor(CVDCRHD4), y=AGE80)) + 
+agebox <- ggplot(health, aes(x=chd, y=age)) + 
   geom_boxplot(outlier.shape = 8) +
   labs(x = "Heart Disease Status", y = "Age", title = "Comparative Boxplots") +
   coord_flip()
 ggsave("figures/agebox.png", plot = agebox)
 
-age.logit = glm(CVDCRHD4 ~ AGE80, family=binomial, data=age_heart)
-summary(age.logit)
+# age.logit = glm(chd ~ age, family=binomial, data=health)
+# summary(age.logit)
+
+# smoke_heart <- health %>% 
+#   filter(chd == 1 | chd == 2) %>% 
+#   mutate(chd = ifelse(chd == 1, 1, 0)) %>% 
+#   mutate(chd = factor(chd)) %>% 
+#   select(chd, SMOKER3) %>% 
+#   filter(!(SMOKER3 == 9)) %>% 
+#   mutate(SMOKER3 = factor(SMOKER3))
+# ggplot(smoke_heart, aes(fill=chd, y=mean(as.numeric(chd)-1), x=SMOKER3)) + 
+#   geom_bar(position="fill", stat="identity")
+
+# sex_heart <- health %>% 
+#   filter(chd == 1 | chd == 2) %>% 
+#   mutate(chd = ifelse(chd == 1, 1, 0)) %>% 
+#   mutate(chd = factor(chd)) %>%
+#   mutate(SEX = factor(SEX)) %>%
+#   select(chd, SEX)
+# ggplot(sex_heart, aes(fill=chd, y=mean(as.numeric(chd)-1), x=SEX)) + 
+#   geom_bar(position="fill", stat="identity")
+# 
+# mean(sex_heart %>% filter(SEX == "1") %>% pull(chd) %>% as.numeric() - 1)
+# mean(sex_heart %>% filter(SEX == "2") %>% pull(chd) %>% as.numeric() - 1)
 
 
-smoke_heart <- health %>% 
-  filter(CVDCRHD4 == 1 | CVDCRHD4 == 2) %>% 
-  mutate(CVDCRHD4 = ifelse(CVDCRHD4 == 1, 1, 0)) %>% 
-  mutate(CVDCRHD4 = factor(CVDCRHD4)) %>% 
-  select(CVDCRHD4, SMOKER3) %>% 
-  filter(!(SMOKER3 == 9)) %>% 
-  mutate(SMOKER3 = factor(SMOKER3))
-ggplot(smoke_heart, aes(fill=CVDCRHD4, y=mean(as.numeric(CVDCRHD4)-1), x=SMOKER3)) + 
-  geom_bar(position="fill", stat="identity")
+# alc_heart <- health %>% 
+#   filter(chd == 1 | chd == 2) %>% 
+#   mutate(chd = ifelse(chd == 1, 1, 0)) %>% 
+#   mutate(chd = factor(chd)) %>%
+#   mutate(RFDRHV5 = factor(RFDRHV5)) %>%
+#   select(chd, RFDRHV5) %>% 
+#   filter(!(RFDRHV5 == 9))
+# ggplot(alc_heart, aes(fill=chd, y=mean(as.numeric(chd)-1), x=RFDRHV5)) + 
+#   geom_bar(position="fill", stat="identity")
 
-
-sex_heart <- health %>% 
-  filter(CVDCRHD4 == 1 | CVDCRHD4 == 2) %>% 
-  mutate(CVDCRHD4 = ifelse(CVDCRHD4 == 1, 1, 0)) %>% 
-  mutate(CVDCRHD4 = factor(CVDCRHD4)) %>%
-  mutate(SEX = factor(SEX)) %>%
-  select(CVDCRHD4, SEX)
-ggplot(sex_heart, aes(fill=CVDCRHD4, y=mean(as.numeric(CVDCRHD4)-1), x=SEX)) + 
-  geom_bar(position="fill", stat="identity")
-
-mean(sex_heart %>% filter(SEX == "1") %>% pull(CVDCRHD4) %>% as.numeric() - 1)
-mean(sex_heart %>% filter(SEX == "2") %>% pull(CVDCRHD4) %>% as.numeric() - 1)
-
-
-alc_heart <- health %>% 
-  filter(CVDCRHD4 == 1 | CVDCRHD4 == 2) %>% 
-  mutate(CVDCRHD4 = ifelse(CVDCRHD4 == 1, 1, 0)) %>% 
-  mutate(CVDCRHD4 = factor(CVDCRHD4)) %>%
-  mutate(RFDRHV5 = factor(RFDRHV5)) %>%
-  select(CVDCRHD4, RFDRHV5) %>% 
-  filter(!(RFDRHV5 == 9))
-ggplot(alc_heart, aes(fill=CVDCRHD4, y=mean(as.numeric(CVDCRHD4)-1), x=RFDRHV5)) + 
-  geom_bar(position="fill", stat="identity")
-
-
-fruit_heart <- health %>% 
-  filter(CVDCRHD4 == 1 | CVDCRHD4 == 2) %>% 
-  mutate(CVDCRHD4 = ifelse(CVDCRHD4 == 1, 1, 0)) %>% 
-  mutate(CVDCRHD4 = factor(CVDCRHD4)) %>%
-  select(CVDCRHD4, FRUTSUM) %>% 
-  filter(FRUTSUM < 500)
-fruitbox <- ggplot(fruit_heart, aes(x=factor(CVDCRHD4), y=FRUTSUM/100)) + 
+fruitbox <- ggplot(health %>% filter(fruit<500), aes(x=chd, y=fruit/100)) + 
   geom_boxplot(outlier.shape = 8) +
   labs(x = "Heart Disease Status", y = "Total Fruit Consumed per Day", title = "Comparative Boxplots") +
   coord_flip()
+ggsave("figures/fruitbox.png", plot = fruitbox)
 
-
-library(naniar)
-sample <- health %>% select(FRUTSUM, AGE80, SMOKER3, RFDRHV5, CVDCRHD4) %>% sample_n(10000)
-
-sample[which(sample$CVDCRHD4 == 7),"CVDCRHD4"] <- NA
-sample[which(sample$CVDCRHD4 == 9),"CVDCRHD4"] <- NA
-sample[which(sample$SMOKER3 == 9),"CVDCRHD4"] <- NA
-
-vis_miss(sample, warn_large_data = F)
-
-
-
-bmi_heart <- health %>% 
-  filter(CVDCRHD4 == 1 | CVDCRHD4 == 2) %>% 
-  select(CVDCRHD4, BMI5) %>% 
-  mutate(CVDCRHD4 = ifelse(CVDCRHD4 == 1, 1, 0)) %>% 
-  mutate(CVDCRHD4 = factor(CVDCRHD4)) %>% 
-  filter(BMI5 < 5000)
-
-bmibox <- ggplot(bmi_heart, aes(x=factor(CVDCRHD4), y=BMI5/100)) + 
+bmibox <- ggplot(health, aes(x=chd, y=bmi)) + 
   geom_boxplot(outlier.shape = 8) +
   labs(x = "Heart Disease Status", y = "BMI", title = "Comparative Boxplots") +
   coord_flip()
+ggsave("figures/bmibox.png", plot = bmibox)
 
 
-ggplot(health %>% sample_n(1000) %>% select(PAMIN11, BMI5), 
-       aes(x=PAMIN11, y=BMI5/100)) +
-  geom_point()
+# ggplot(health %>% sample_n(1000) %>% select(cardio_freq, bmi) %>% filter(cardio_freq < 600), 
+#        aes(x=cardio_freq, y=bmi/100)) +
+#   geom_point()
+# 
+# ggplot(health %>% sample_n(1000), aes(age, bmi/100, color = factor(diabetes))) +
+#   geom_point()
+
+
+ct <- table(health$smoking, health$marital_status)
+res.ca <- CA(ct, graph = FALSE)
+fviz_ca_biplot(res.ca, repel = TRUE)
+
+smoking_mosaic <- ggplot(data = health %>% filter(!(is.na(smoking)))) +
+  geom_mosaic(aes(x = product(chd, smoking), fill=chd)) +
+  theme(text = element_text(size=7))
+ggsave("figures/smoking_mosaic.png", plot=smoking_mosaic, width = 6, height = 4)
+
+agesmokebox <- ggplot(health %>% filter(!(is.na(smoking)))) + 
+  geom_boxplot(aes(x=smoking, y=age)) + 
+  labs(x="Smoking category", y="Age", title="Comparative Boxplots") +
+  scale_x_discrete(labels=c("Never", "Former", "Some Days", "Every Day"))
+ggsave("figures/agesmokebox.png", plot=agesmokebox)
+
+heart_attack_mosaic <- ggplot(data = health %>% filter(!(is.na(heart_attack)))) +
+  geom_mosaic(aes(x = product(heart_attack, chd), fill=heart_attack))
+ggsave("figures/heart_attack_mosaic.png", plot=heart_attack_mosaic)
+
+
