3830b598ae27a90c4f8996bfb455d23e93b86e71
Anil Anderson <aniland@unc.edu>
2022-12-04 14:34:00 -0500

    Produced final dimensionality reduction plots for continuous and categorical variables.

--- dim_reduce.R
+++ dim_reduce.R
@@ -8,26 +8,32 @@ str(health)
 dummy_cols(health %>% na.omit, select_columns = c('general_health'), remove_selected_columns = TRUE)
 as.numeric(health$sex)
 
-smpl_num <- health %>% select_if(is.numeric) %>% na.omit %>% sample_n(10000)
-r <- prcomp(smpl_num, center = T, scale. = T)
+set.seed(400)
+smpl_num <- health %>% select("days_poor_phys_health", 
+                              "days_poor_ment_health", 
+                              "age", "bmi", "alcohol_freq", 
+                              "fruit", "vegetable", "cardio_freq", 
+                              "strength_freq", "chd") %>% 
+  na.omit %>% sample_n(30000)
+r <- prcomp(smpl_num %>% select(-chd), center = T, scale. = T)
 r
 summary(r)
 
 ggplot(r$x %>% as_tibble() %>% select(PC1, PC2), aes(PC1, PC2)) +
-  geom_point()
+  geom_point(aes(col = smpl_num$chd, alpha = smpl_num$chd)) + 
+  scale_alpha_manual(guide='none', values = c(0.1, 0.5)) +
+  labs(color = "Heart Disease")
 
-smpl_cat <- health %>% select_if(~class(.) == 'factor') %>% na.omit %>% sample_n(10000)
+
+summary(r$x[which(smpl_num$chd == 1),1:2])
+summary(r$x[which(smpl_num$chd == 0),1:2])
+
+r$rotation[,1:2]
+
+
+smpl_cat <- health %>% select_if(~class(.) == 'factor') %>% na.omit %>% sample_n(30000)
 res.mca = MCA(smpl_cat)
-plot.MCA(res.mca, invisible=c("var","quali.sup"), cex=0.7)
-plot.MCA(res.mca, invisible=c("ind","quali.sup"), cex=0.7)
-plot.MCA(res.mca, invisible=c("ind"))
-plot.MCA(res.mca, invisible=c("ind", "var"))
-
-
-
-as.numeric(health %>% select_if(~class(.) == 'factor'))
-indx <- sapply(health, is.factor)
-lapply(health[indx], function(x) as.numeric(as.character(x)))  
+plot.MCA(res.mca, invisible=c("ind","quali.sup"), cex=0.5)
   
   
   
