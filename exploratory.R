11e89f24174e0c3b08390c8e5e813f5bdec7614d
Anil Anderson <aniland@unc.edu>
2022-12-05 13:50:39 -0500

    Fixed select commands because MASS masks select from dplyr.

--- exploratory.R
+++ exploratory.R
@@ -3,7 +3,7 @@ source("setup.R")
 agehist <- ggplot(health, aes(age)) + 
   geom_histogram(binwidth = 5) + 
   labs(x = "Age", y = "Count", title = "Histogram of Ages")
-ggsave("figures/agehist.png", plot = agehist, dpi = 300)
+ggsave("figures/agehist.png", plot = agehist, dpi=300)
 # ggplot(health %>% filter(!(is.na(age_cat))), aes(age_cat)) + geom_bar()
 
 agebox <- ggplot(health, aes(x=chd, y=age)) + 
@@ -19,7 +19,7 @@ ggsave("figures/agebox.png", plot = agebox)
 #   filter(chd == 1 | chd == 2) %>% 
 #   mutate(chd = ifelse(chd == 1, 1, 0)) %>% 
 #   mutate(chd = factor(chd)) %>% 
-#   select(chd, SMOKER3) %>% 
+#   dplyr::select(chd, SMOKER3) %>% 
 #   filter(!(SMOKER3 == 9)) %>% 
 #   mutate(SMOKER3 = factor(SMOKER3))
 # ggplot(smoke_heart, aes(fill=chd, y=mean(as.numeric(chd)-1), x=SMOKER3)) + 
@@ -30,7 +30,7 @@ ggsave("figures/agebox.png", plot = agebox)
 #   mutate(chd = ifelse(chd == 1, 1, 0)) %>% 
 #   mutate(chd = factor(chd)) %>%
 #   mutate(SEX = factor(SEX)) %>%
-#   select(chd, SEX)
+#   dplyr::select(chd, SEX)
 # ggplot(sex_heart, aes(fill=chd, y=mean(as.numeric(chd)-1), x=SEX)) + 
 #   geom_bar(position="fill", stat="identity")
 # 
@@ -43,12 +43,12 @@ ggsave("figures/agebox.png", plot = agebox)
 #   mutate(chd = ifelse(chd == 1, 1, 0)) %>% 
 #   mutate(chd = factor(chd)) %>%
 #   mutate(RFDRHV5 = factor(RFDRHV5)) %>%
-#   select(chd, RFDRHV5) %>% 
+#   dplyr::select(chd, RFDRHV5) %>% 
 #   filter(!(RFDRHV5 == 9))
 # ggplot(alc_heart, aes(fill=chd, y=mean(as.numeric(chd)-1), x=RFDRHV5)) + 
 #   geom_bar(position="fill", stat="identity")
 
-fruitbox <- ggplot(health %>% filter(fruit<500), aes(x=chd, y=fruit/100)) + 
+fruitbox <- ggplot(health %>% filter(fruit<5), aes(x=chd, y=fruit)) + 
   geom_boxplot(outlier.shape = 8) +
   labs(x = "Heart Disease Status", y = "Total Fruit Consumed per Day", title = "Comparative Boxplots") +
   coord_flip()
@@ -61,7 +61,7 @@ bmibox <- ggplot(health, aes(x=chd, y=bmi)) +
 ggsave("figures/bmibox.png", plot = bmibox)
 
 
-# ggplot(health %>% sample_n(1000) %>% select(cardio_freq, bmi) %>% filter(cardio_freq < 600), 
+# ggplot(health %>% sample_n(1000) %>% dplyr::select(cardio_freq, bmi) %>% filter(cardio_freq < 600), 
 #        aes(x=cardio_freq, y=bmi/100)) +
 #   geom_point()
 # 
@@ -88,7 +88,9 @@ ggplot(health %>% filter(!(is.na(smoking)))) +
   geom_mosaic(aes(x = product(smoking, age_cat), fill=smoking))
 
 heart_attack_mosaic <- ggplot(data = health %>% filter(!(is.na(heart_attack)))) +
-  geom_mosaic(aes(x = product(heart_attack, chd), fill=heart_attack))
+  geom_mosaic(aes(x = product(heart_attack, chd), fill=heart_attack)) +
+  coord_fixed(ratio = 0.8) +
+  labs(y = "Heart Attack", x = "CHD")
 ggsave("figures/heart_attack_mosaic.png", plot=heart_attack_mosaic)
 
 cor(health$cardio_freq, health$strength_freq, use = "complete.obs")
